78467717d67d4639befab5fdfa6b55c3d1359449
Krzysztof Witos <kwitos89@gmail.com>
2022-12-30 09:45:13 +0100

    add header

---
+++ js/app.js
@@ -0,0 +1,73 @@
+const burger = document.querySelector('.nav__burgerBtn');
+const menu = document.querySelector('.nav__menu');
+const searchBox = document.querySelector('.nav__serachBox');
+const searchBtn = document.querySelector('.nav__searchBtn');
+
+
+//  Animaited Button Burger //
+
+const clickBurgerBtn = () => {
+    if (burger.classList.contains('active')) {
+
+        burger.classList.toggle('no__active')
+
+    } else {
+
+        burger.classList.add('active')
+    };
+};
+
+// Open Mobile Menu //
+
+const openMenu = () => {
+    menu.classList.toggle('active__menu')
+
+    clickBurgerBtn()
+}
+
+// Close Menu Mobile Click Outside //
+
+const closeOutside = (e) => {
+
+    if (menu.classList.contains('active__menu')) {
+
+        if (!menu.contains(e.target))
+
+            openMenu()
+
+        if (burger.contains(e.target))
+            openMenu()
+    }
+}
+
+//  Close Menu When Chang Siaze Screen //
+
+const resize = () => {
+    if (this.innerWidth >= 930) {
+        openMenu()
+    }
+}
+
+//  Open Search Box //
+
+const openSearch = () => {
+    searchBox.classList.toggle('nav__serachBox--active');
+};
+
+// Close SearchBox Click Outsie //
+
+const closeOutsideSearchBox = (e) => {
+
+    if (searchBox.classList.contains('nav__serachBox--active')) {
+
+        if (!searchBox.contains(e.target))
+
+            openSearch()
+    }
+}
+
+window.addEventListener('resize', resize);
+document.addEventListener('mouseup', closeOutsideSearchBox);
+searchBtn.addEventListener('click', openSearch);
+document.addEventListener('mouseup', closeOutside);
+burger.addEventListener('click', openMenu);

3d9076481ca9e6c7b43cdcb4b868a4c07c3f1c6f
Krzysztof Witos <kwitos89@gmail.com>
2023-01-11 17:42:14 +0100

    add fuction dragabel slider for images section

--- js/app.js
+++ js/app.js
@@ -4,6 +4,88 @@ const searchBox = document.querySelector('.nav__serachBox');
 const searchBtn = document.querySelector('.nav__searchBtn');
 const bodyEl = document.body;
 
+const slider = document.querySelector('.images__sliderBox');
+const innerSlider = document.querySelector('.images__innerList');
+const sliderItems = document.querySelectorAll('.images__items');
+
+
+//  Dragablr Slider Fuction For Images Section //
+let pressed = false;
+let startx;
+let x;
+
+const mouseDown = (e) => {
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = 'grabbing'
+};
+const mouseEnter = () => {
+    slider.style.cursor = 'grab'
+};
+
+
+const mouseUp = () => {
+    slider.style.cursor = 'grab'
+    
+};
+
+const press = () => {
+    pressed = false;
+}
+
+const mouseMove = (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`
+
+    checkBoundry();
+};
+
+const checkBoundry = () => {
+    let outer = slider.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if (parseInt(innerSlider.style.left) > 0) {
+        innerSlider.style.left = '0px'
+    } else if (inner.right < outer.right) {
+        innerSlider.style.left = `-${inner.width - outer.width}px`
+    }
+};
+
+slider.addEventListener('mousedown', mouseDown);
+slider.addEventListener('mouseenter', mouseEnter);
+slider.addEventListener('mouseup', mouseUp);
+slider.addEventListener('mousemove', mouseMove);
+window.addEventListener('mouseup', press);
+
+//  Mobile Fuction //
+const touchStart = (e) => {
+    pressed = true;
+    startx = e.touches[0].clientX - innerSlider.offsetLeft;
+    slider.style.cursor = 'grabbing';
+};
+
+const touchMove = (e) => {
+    if (!pressed) return;
+    e.preventDefault();
+    x = e.touches[0].clientX;
+    innerSlider.style.left = `${x - startx}px`;
+    checkBoundry();
+};
+
+const touchEnd = () => {
+    pressed = false;
+    slider.style.cursor = 'grab'
+};
+
+
+slider.ontouchstart = touchStart;
+slider.ontouchmove = touchMove;
+slider.ontouchend = touchEnd;
+
 
 
 //  Animaited Button Burger //
